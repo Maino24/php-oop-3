c0ddb543dcd131d95042a5214ffdd1b2a088474b
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-22 17:29:32 +0100

    index

--- index.php
+++ index.php
@@ -17,36 +17,14 @@ $prodotti = [
   new audioLibro('La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 120, 'spotify', 'https://spotify-libri.com'),
 ];
 
-//$prodotti[0]->setAnno(2005);
-
-// $libbro1 = new Libro(24, 'flessibile');
-// var_dump($libbro1);
-// $libbro1->nome = "il signore degli anelli";
-// var_dump($libbro1);
 
 var_dump( $prodotti );
 
-$prodotti[0]->getStringCustom();
-
-
-$impiegato = new Impiegato('Marco', 'Rossi', 3);
-
-var_dump($impiegato);
-
-$impiegato->anno = 1998;
-
-var_dump($impiegato);
-
-$impiegato->getStringCustom();
 
 
 
-
-
-
-
-// foreach( $prodotti as $elem ){
-//   echo get_class($elem);
-//   echo "<br>";
-// }
+ foreach( $prodotti as $elem ){
+    echo get_class($elem);
+    echo "<br>";
+ }
 ?>
